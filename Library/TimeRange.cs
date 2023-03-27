5b87a28ceaa75928afeac3964fe756951d0b96b3
c3n9 <arseniy11041104@gmail.com>
2023-03-28 00:06:35 +0300

    full library

---
+++ Library/TimeRange.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Library
+{
+    public class TimeRange
+    {
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+
+        public bool IsCoversRange(TimeRange range)
+        {
+            return StartTime < range.EndTime && range.StartTime < EndTime;
+        }
+
+        public override string ToString()
+        {
+            return $"{StartTime:hh\\:mm}-{EndTime:hh\\:mm}";
+        }
+    }
+}
